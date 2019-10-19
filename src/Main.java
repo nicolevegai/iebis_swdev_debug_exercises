1a0f135214e8302cbba77e510b22baede107e679
nicolevegai <mvega.ieu2017@student.ie.edu>
2019-10-20 01:38:35 +0200

    Exercize 1, 2 y empezar 3 aunque 3 tiene error

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,37 @@ public class Main {
 
     public static void main(String[] args) {
         WordAnalyzer wordAnalyzer = new WordAnalyzer("hello");
+        System.out.println("a)");
         System.out.println(wordAnalyzer.firstRepeatedCharacter());          // expected output: l
         System.out.println(wordAnalyzer.countGroupsRepeatedCharacters());   // expected output: 1
+        System.out.println();
+
+        wordAnalyzer = new WordAnalyzer("hoollaa");
+        System.out.println("b)");
+        System.out.println(wordAnalyzer.firstRepeatedCharacter());          // expected output: o
+        System.out.println(wordAnalyzer.countGroupsRepeatedCharacters());   // expected output: 3
+        System.out.println();
 
         wordAnalyzer = new WordAnalyzer("comprehensive");
+        System.out.println("c)");
         System.out.println(wordAnalyzer.firstMultipleCharacter());          // expected output: e
         System.out.println(wordAnalyzer.firstRepeatedCharacter());          // expected output:
+        System.out.println();
 
         wordAnalyzer = new WordAnalyzer("development");
+        System.out.println("d)");
         System.out.println(wordAnalyzer.firstRepeatedCharacter());          // expected output:
         System.out.println(wordAnalyzer.countGroupsRepeatedCharacters());   // expected output: 0
+        System.out.println();
 
         wordAnalyzer = new WordAnalyzer("mississippi!!!");
+        System.out.println("e)");
         System.out.println(wordAnalyzer.firstMultipleCharacter());          // expected output: i
         System.out.println(wordAnalyzer.countGroupsRepeatedCharacters());   // expected output: 4
+        System.out.println();
 
         wordAnalyzer = new WordAnalyzer("aabbcdaaaabb");
+        System.out.println("f)");
         System.out.println(wordAnalyzer.countGroupsRepeatedCharacters());   // expected output: 4
         System.out.println(wordAnalyzer.firstRepeatedCharacter());          // expected output: a
         System.out.println(wordAnalyzer.firstMultipleCharacter());          // expected output: a
--- src/WordAnalyzer.java
+++ src/WordAnalyzer.java
@@ -1,8 +1,12 @@
+import com.sun.tools.jdi.SocketAttachingConnector;
+
+import java.sql.SQLOutput;
+
 /**
  * A class that analyzes words.
  */
-public class WordAnalyzer
-{
+public class WordAnalyzer{
+
     /**
      * Constructs an analyzer for a given word.
      * @param aWord the word to be analyzed
@@ -23,12 +27,26 @@ public class WordAnalyzer
         for (int i = 0; i < word.length(); i++)
         {
             char ch = word.charAt(i);
+            /**
+             * 2.
+             * we need to add this line of code so when there are no repeated characters
+             * the program knows what to do.
+             * In this case print [] so the Exception is not sent.
+             */
+
+           if ((i+1) == word.length()){ // Si ningun character se repite pasa esto
+            return 0;
+       }else
+            {
+
             if (ch == word.charAt(i + 1))
-                return ch;
+            return ch;
+        }
         }
         return 0;
-    }
 
+
+    }
     /**
      * Gets the first multiply occuring character. A character is <i>multiple</i>
      * if it occurs at least twice in the word, not necessarily in adjacent positions.
@@ -48,7 +66,10 @@ public class WordAnalyzer
 
     private int find(char c, int pos)
     {
-        for (int i = pos; i < word.length(); i++)
+        //1.
+        // We need to add 1 to pos because if not the first letter or the char that is written
+        // in  if (find(ch, i) >= 0) first multiple character method is always going to be the value of pos.
+        for (int i = pos + 1; i < word.length(); i++)
         {
             if (word.charAt(i) == c)
             {
@@ -63,19 +84,28 @@ public class WordAnalyzer
      * four such groups: ss, ss, pp and !!!.
      * @return the number of repeated character groups
      */
-    public int countGroupsRepeatedCharacters()
-    {
+    public int countGroupsRepeatedCharacters() {
         int c = 0;
-        for (int i = 1; i < word.length() - 1; i++)
+        /** As the loop starts in int i=1 it does not check repeated characters in the first two chars
+         * Therefore we have to change the condition to i=0 so it checks the first chars/
+         */
+        for (int i = 0; i < word.length() - 1; i++)
+        //Now, the program shows a thread Exception
         {
             if (word.charAt(i) == word.charAt(i + 1)) // found a repetition
             {
-                if (word.charAt(i - 1) != word.charAt(i)) // it't the start
+            if (i != 0) {
+                (word.charAt(i - 1) != word.charAt(i)) // it't the start
+                c++;
+                } else
+                    {
                     c++;
+                    }
             }
+                }
+            return c;
         }
-        return c;
-    }
+
 
     private String word;
 }
